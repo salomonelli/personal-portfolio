21de6854330457c36a5387639804d37235b1e8db
Sara Steiert <sara.steiert@outlook.com>
2017-04-29 22:22:08 +0200

    ADD meta description

--- src/App.js
+++ src/App.js
@@ -18,70 +18,72 @@ import ContactComponent from './components/contact/contact';
 import FooterComponent from './components/footer/footer';
 
 class App extends Component {
-    render() {
-        return (
-            <MuiThemeProvider>
-                <div className="App">
-                  <Helmet>
-                      <meta charSet="utf-8" />
-                      <title>Sara Steiert - Software Developer</title>
-                      <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
-                  </Helmet>
-                    <ProgressComponent></ProgressComponent>
-                    <div className="section header-section">
-                        <div className="section-container header-component">
-                            <HeaderComponent></HeaderComponent>
-                        </div>
-                    </div>
-                    <div className="section colored">
-                        <div className="section-container">
-                            <IntroductionComponent></IntroductionComponent>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="section-container">
-                            <TimelineComponent></TimelineComponent>
-                        </div>
-                    </div>
-                    <div className="section colored">
-                        <div className="section-container">
-                            <ProgrammingComponent></ProgrammingComponent>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="section-container">
-                            <AgileQuoteComponent></AgileQuoteComponent>
-                        </div>
-                    </div>
-                    <div className="section colored">
-                        <div className="section-container">
-                            <AgileComponent></AgileComponent>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="section-container">
-                            <DesignComponent></DesignComponent>
-                        </div>
-                    </div>
-                    <div className="section colored">
-                        <div className="section-container">
-                            <CodeGithubComponent></CodeGithubComponent>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="section-container">
-                            <ContactComponent></ContactComponent>
-                        </div>
-                    </div>
-                    <div className="section colored">
-                        <div className="section-container footer-component">
-                            <FooterComponent></FooterComponent>
-                        </div>
-                    </div>
-                </div>
-            </MuiThemeProvider>
-        );
-    }
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="App">
+          <Helmet>
+            <meta charSet="utf-8"/>
+            <title>Sara Steiert - Software Developer</title>
+            <meta name="description" content="Sara Steiert - Software Developer. Loves JavaScript, cats and eating. Professional Software Developer."/>
+            <meta name="keywords" content="software developer, web development, javascript, react, material ui" />
+            <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no"/>
+          </Helmet>
+          <ProgressComponent></ProgressComponent>
+          <div className="section header-section">
+            <div className="section-container header-component">
+              <HeaderComponent></HeaderComponent>
+            </div>
+          </div>
+          <div className="section colored">
+            <div className="section-container">
+              <IntroductionComponent></IntroductionComponent>
+            </div>
+          </div>
+          <div className="section">
+            <div className="section-container">
+              <TimelineComponent></TimelineComponent>
+            </div>
+          </div>
+          <div className="section colored">
+            <div className="section-container">
+              <ProgrammingComponent></ProgrammingComponent>
+            </div>
+          </div>
+          <div className="section">
+            <div className="section-container">
+              <AgileQuoteComponent></AgileQuoteComponent>
+            </div>
+          </div>
+          <div className="section colored">
+            <div className="section-container">
+              <AgileComponent></AgileComponent>
+            </div>
+          </div>
+          <div className="section">
+            <div className="section-container">
+              <DesignComponent></DesignComponent>
+            </div>
+          </div>
+          <div className="section colored">
+            <div className="section-container">
+              <CodeGithubComponent></CodeGithubComponent>
+            </div>
+          </div>
+          <div className="section">
+            <div className="section-container">
+              <ContactComponent></ContactComponent>
+            </div>
+          </div>
+          <div className="section colored">
+            <div className="section-container footer-component">
+              <FooterComponent></FooterComponent>
+            </div>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
