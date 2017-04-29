89267112c031954accfa19d2c093cd2c9cc3d0c7
Sara Steiert <sara.steiert@outlook.com>
2017-04-29 23:08:41 +0200

    ADD agile text

--- src/components/agile/agile.jsx
+++ src/components/agile/agile.jsx
@@ -8,8 +8,14 @@ class AgileComponent extends Component {
         return (
           <div className="agile">
               <p className="headline">Agile development</p>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              <p className="text-justify">
+                Excellent results can be achieved through agility.
+                Therefore I try to empower the agile concept in any aspects of life.
+                I believe that brilliant software can only be achieved through constant feedback and an open mind.
+                Self-organization of teams triggers creativity.
+                <br /> <br />
+                Through working experience, I have become an expert on Scrum.
+                I have worked with JIRA, Confluence and classic tools like post-its.
               </p>
           </div>
         );
