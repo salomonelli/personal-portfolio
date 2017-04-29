9cd726b30aa84630abf7dbbf6b3d8003038cb8b9
Sara Steiert <sara.steiert@outlook.com>
2017-04-29 17:44:30 +0200

    ADD syntax highlighting

--- src/components/programming/programming.jsx
+++ src/components/programming/programming.jsx
@@ -25,41 +25,65 @@ class ProgrammingComponent extends Component {
                         </div>
                         <div className="console-content">
                             <ul>
-                                <li>&lt;html&gt;</li>
+                                <li>&lt;<span className="html-1">html</span>&gt;</li>
                                 <li>
                                   <ul className="no-top-padding">
-                                    <li>&lt;head&gt;</li>
                                     <li>
-                                      <ul className="no-top-padding">
-                                        <li>&lt;title&gt;</li>
-                                          <ul className="no-top-padding">
-                                            <li>Programming skills</li>
-                                          </ul>
-                                        <li>&lt;/title&gt;</li>
-                                      </ul>
+                                      &lt;<span className="html-1">head</span>&gt;
+                                      &lt;/<span className="html-1">head</span>&gt;
                                     </li>
-                                    <li>&lt;/head&gt;</li>
-                                    <li>&lt;body&gt;</li>
+                                    <li>&lt;<span className="html-1">body</span>&gt;</li>
                                     <li>
                                       <ul className="no-top-padding">
-                                        <li>&lt;ul&gt;</li>
+                                        <li>&lt;<span className="html-1">ul</span>&gt;</li>
                                         <ul className="no-top-padding">
-                                          <li>&lt;li&gt; ES.next &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; Node.js &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; React &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; Angular 2 &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; Docker &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; Linux &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; LESS, SASS &lt;/li&gt;</li>
-                                          <li>&lt;li&gt; HTML5, CSS3 &lt;/li&gt;</li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">ES.next</span>
+                                            &lt;/<span className="html-1">li</span>&gt;</li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">Node.js</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">React</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">Angular 2</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">Docker</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">Linux</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">LESS, SASS</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
+                                          <li>
+                                            &lt;<span className="html-1">li</span>&gt;
+                                            <span className="html-2">HTML5, CSS3</span>
+                                            &lt;/<span className="html-1">li</span>&gt;
+                                          </li>
                                         </ul>
-                                        <li>&lt;/ul&gt;</li>
+                                        <li>&lt;/<span className="html-1">ul</span>&gt;</li>
                                       </ul>
                                     </li>
-                                    <li>&lt;/body&gt;</li>
+                                    <li>&lt;/<span className="html-1">body</span>&gt;</li>
                                   </ul>
                                 </li>
-                                <li>&lt;/html&gt;</li>
+                                <li>&lt;/<span className="html-1">html</span>&gt;</li>
                             </ul>
                         </div>
                     </Paper>
