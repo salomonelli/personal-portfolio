a41b3fb0968e2adf63dc396e23f84d002a937f1b
Sara Steiert <sara.steiert@outlook.com>
2017-04-29 23:33:33 +0200

    REMOVE index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
-import './index.css';
 
 injectTapEventPlugin();
 ReactDOM.render(
