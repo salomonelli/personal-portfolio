59d0a9b731bfcf8b0a573ea2b68fa502703bc010
Sara Steiert <sara.steiert@outlook.com>
2017-04-30 16:57:46 +0200

    ADD dark voice

--- src/components/dark-voice/dark-voice.jsx
+++ src/components/dark-voice/dark-voice.jsx
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import MicrophoneIcon from 'material-ui/svg-icons/av/mic';
+import MicrophoneOffIcon from 'material-ui/svg-icons/av/mic-off';
+import  {Howl} from 'howler';
 import './dark-voice.css';
+console.log('aaa');
+console.dir(Howl);
+
+
+const darkVoiceSound = new Howl({
+  src: ['dark-voice.mp3']
+});
+
+
 
 
 class DarkVoiceComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          sound: false
+        };
+    }
+
+    disableSound() {
+      this.setState({sound: false});
+      darkVoiceSound.pause();
+    }
+
+    enableSound() {
+      this.setState({sound: true});
+      darkVoiceSound.play();
+      // Fires when the sound finishes playing.
+      darkVoiceSound.on('end', () => {
+        this.disableSound();
+      });
+    }
 
     render() {
         return (
@@ -21,9 +52,17 @@ class DarkVoiceComponent extends Component {
                   </p>
               </p>
               <div className="microphone-action">
-                <FloatingActionButton backgroundColor="#A80202">
-                  <MicrophoneIcon />
-                </FloatingActionButton>
+                { !this.state.sound &&
+                  <FloatingActionButton backgroundColor="#A80202" onTouchTap={this.enableSound.bind(this)}>
+                    <MicrophoneIcon />
+                  </FloatingActionButton>
+                }
+                { this.state.sound &&
+                  <FloatingActionButton backgroundColor="#A80202" onTouchTap={this.disableSound.bind(this)}>
+                    <MicrophoneOffIcon />
+                  </FloatingActionButton>
+                }
+
               </div>
             </div>
         );
